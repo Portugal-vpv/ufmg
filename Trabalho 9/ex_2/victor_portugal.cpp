22b79c384e22dd26e8cbb9b662868fa86259cbe5
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-02-01 19:50:51 -0300

    Initial main function development

---
+++ Trabalho 9/ex_2/victor_portugal.cpp
@@ -0,0 +1,18 @@
+// Victor Portugal de Vasoncellos
+// Trabalho 9 de POO ex 2
+
+
+/**
+ * Para rodar os testes do execício 2 basta rodar o comando
+ * g++ victor_portugal.cpp
+ * **/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout<<"Its working"<<endl;
+    return 0;
+}
