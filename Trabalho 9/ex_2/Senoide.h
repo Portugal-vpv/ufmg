fde2f60fe9e82be15e4055a654c4d3d29c704778
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-02-01 20:16:51 -0300

    Sine header file

--- Trabalho 9/ex_2/Senoide.h
+++ Trabalho 9/ex_2/Senoide.h
@@ -3,6 +3,9 @@
 #ifndef SENOIDAL_H
 #define SENOIDAL_H
     class Senoide: public Funcao {
-        
+        public:
+            Senoide(int input): Funcao(input) {};
+            ~Senoide(){};
+            double getIntegral();
     };
 #endif //SENOIDAL_H
