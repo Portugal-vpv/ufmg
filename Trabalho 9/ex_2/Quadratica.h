ee6208365eaa34a7f6916875423e156bc15b9a2c
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-02-01 20:12:08 -0300

    Quadratica class header file development.

---
+++ Trabalho 9/ex_2/Quadratica.h
@@ -0,0 +1,15 @@
+#include "Funcao.h"
+
+#ifndef QUADRATICA_H
+#define QUADRATICA_H
+    class Quadratica: public Funcao {
+        private:
+            int _a, _b, _c;
+        
+        public:
+            Quadratica(int input, int a, int b, int c): Funcao(input), _a(a), _b(b), _c(c) {};
+            ~Quadratica(){};
+            double getIntegral();
+
+    };
+#endif
