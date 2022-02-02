0825569452a99a41587f913479de0835fec08c90
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-02-01 21:05:34 -0300

    Change Quadratica tipes on private members and virtual functio getIntegral.

--- Trabalho 9/ex_2/Quadratica.h
+++ Trabalho 9/ex_2/Quadratica.h
@@ -4,12 +4,12 @@
 #define QUADRATICA_H
     class Quadratica: public Funcao {
         private:
-            int _a, _b, _c;
+            double _a, _b, _c;
         
         public:
-            Quadratica(int input, int a, int b, int c): Funcao(input), _a(a), _b(b), _c(c) {};
+            Quadratica(double a, double b, double c):  _a(a), _b(b), _c(c) {};
             ~Quadratica(){};
-            double getIntegral(int p0, int pn, int intervalo);
+            double getIntegral(double p0, double pn, double intervalo);
 
     };
 #endif
